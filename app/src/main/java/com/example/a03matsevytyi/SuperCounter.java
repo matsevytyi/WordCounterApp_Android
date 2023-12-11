4dde80ce4e44f009271ce011f3ea92e7a3e386ab
andriimatsevytyi <macevitiya@i.ua>
2023-12-11 11:46:22 -0500

    updated the SuperCounter class so that it handles counting better. Initially words were count correctly (because words usually finish either with space or with coma/dot/etc. and space), but I updated it to handle cases like word1/word2. Also, I updated CharCounter to count only figures and letters, because this is usually count (in Word for example).

--- app/src/main/java/com/example/a03matsevytyi/SuperCounter.java
+++ app/src/main/java/com/example/a03matsevytyi/SuperCounter.java
@@ -4,13 +4,22 @@ public class SuperCounter {
     int countWords(String inpt){
         int count = 1;
         for(int i = 2; i < inpt.length(); i++){
-            if(inpt.charAt(i-1) == ' ' && inpt.charAt(i-2) != ' ' && inpt.charAt(i) != ' ')
+            if((inpt.charAt(i-1) == ' ' && inpt.charAt(i-2) != ' ' && inpt.charAt(i) != ' ')
+                    || (inpt.charAt(i-2) >= '!' && inpt.charAt(i-2) <= '/')
+                    ||(inpt.charAt(i-2) > ':' && inpt.charAt(i-2) <= '@'))
                 count++;
         }
         return count;
     }
 
     int countChar(String inpt){
-        return inpt.length();
+        inpt = inpt.toLowerCase();
+        int result = 0;
+        for(int i = 0; i < inpt.length(); i++){
+            if((inpt.charAt(i) >= 'a' && inpt.charAt(i) <= 'z')
+                || (inpt.charAt(i) >= '0' && inpt.charAt(i) <= '9'))
+                result++;
+        }
+        return result;
     }
 }
